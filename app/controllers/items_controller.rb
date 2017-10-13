93f0c9c303f6eccee39b1112e235ea5b47c9d806
mao hung <maohung71@gmail.com>
2017-10-12 21:05:07 -0400

    2 controller routes with crud

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,64 @@
 class ItemsController < ApplicationController
+
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
+
   def index
+    @items = Item.all.order("created_at DESC")
   end
 
   def show
   end
 
-  def add
+  def new
+    @item = Item.new
+    # @list = @item.list
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    # @hobby = Hobby.find(params[:hobby_id])
+    # @item = @hobby.items.new(item_params)
+
+    respond_to do |format|
+      if @item.save
+        format.html { redirect_to @item, notice: 'item was successfully created.' }
+        format.json { render :show, status: :created, location: @item }
+      else
+        format.html { render :new }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
   end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @item.destroy
+    respond_to do |format|
+      format.html { redirect_to items_path, notice: 'Item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
+
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,7 @@
 class ListsController < ApplicationController
 
+  before_action :find_list, only: [:show, :edit, :update, :destroy]
+
   def index
     @lists = List.all.order("created_at DESC")
   end
@@ -17,7 +19,7 @@ class ListsController < ApplicationController
 
     respond_to do |format|
       if @list.save
-        format.html { redirect_to @list, notice: 'list was successfully created.' }
+        format.html { redirect_to root_path, notice: 'list was successfully created.' }
         format.json { render :show, status: :created, location: @list }
       else
         format.html { render :new }
@@ -27,6 +29,32 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if @list.update(list_params)
+      redirect_to list_path(@list)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @list.destroy
+    respond_to do |format|
+      format.html { redirect_to lists_path, notice: 'Item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :description)
+  end
+
+  def find_list
+    @list = List.find(params[:id])
   end
 
 
